9348a6107a00bbfd8d00deedbeda0cf15aba0ae9
Jaap Versteegh <j.r.versteegh@gmail.com>
2014-07-02 19:15:15 +0200

    - Exercise 3

--- 03-odd-or-even/main.go
+++ 03-odd-or-even/main.go
@@ -2,12 +2,16 @@ package main
 
 import "fmt"
 
+func even(value int) (bool) {
+  return (value % 2) == 0
+}
+
 func main() {
 	n := 13
 
 	if even(n) {
-		fmt.Printf("%v is even!")
+		fmt.Printf("%v is even!\n", n)
 	} else {
-		fmt.Printf("%v is odd!")
+		fmt.Printf("%v is odd!\n", n)
 	}
 }
